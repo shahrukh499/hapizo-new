5c4471ff32fc430f08aef1da2d5eaae1a3a126f1
shahrukh499 <mdshahrukhnov10@gmail.com>
2026-05-08 19:28:50 +0530

    new updates

--- app/utils/apiConfig.ts
+++ app/utils/apiConfig.ts
@@ -66,6 +66,7 @@ const createRequestOptions = (method:any, body = null) => {
     method,
     headers: createHeaders(), // Use createHeaders to get dynamic headers
     redirect: "follow",
+    cache: "force-cache"
   };
 
   if (
